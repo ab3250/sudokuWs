44a9b53ae0e9e107dce61870acb8f71b5d7677c2
AB <johndoe@example.com>
2022-06-02 20:52:54 -0400

    2022_06_02_08_52_PM

---
+++ fifoRWP.sld
@@ -0,0 +1,10 @@
+(define-library (fifoRWP)
+  (import
+    (scheme base))
+(export
+	fifoPoll
+	gwinit
+	scheme_read_ws
+	scheme_write_ws)
+	(include-shared "fifoRWP"))
+
--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 ./stop.sh
 #./gwsocket --addr=127.0.0.1 -p 8080 --pipein=./send --pipeout=./recv &
-./gwsocket-modified &
+./gwsocket --addr=127.0.0.1 -p 8080 --pipein=/tmp/fifoOut --pipeout=/tmp/fifoIn &
 #export NGINX_ROOT="$(pwd)/www/"
 #sudo /usr/local/nginx/sbin/nginx -c $(pwd)/nginx.conf
 #sudo /usr/local/openresty/bin/openresty -c $(pwd)/nginx.conf
